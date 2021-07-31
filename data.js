d8b9936feb8aed269478459d6b4de6556bce5b93
Dhanapriya <87746600+DhanapriyaDeva@users.noreply.github.com>
2021-07-31 13:11:07 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,47 @@
+
+export default [
+    {
+        id:1,
+        name:'Bertie Yates',
+        age:29,
+        image:'./images/img1.png',
+    },
+    {
+        id:2,
+        name:'Bertie Yates',
+        age:29,
+        image:'./images/img2.png',
+    },
+    {
+        id:3,
+        name:'Bertie Yates',
+        age:29,
+        image:'./images/img3.png',
+    },
+    {
+        id:4,
+        name:'Bertie Yates',
+        age:29,
+        image:'./images/img4.png',
+    },
+    {
+        id:5,
+        name:'Bertie Yates',
+        age:29,
+        image:'./images/img5.png',
+    },
+    {
+        id:6,
+        name:'Bertie Yates',
+        age:29,
+        image:'./images/img2.png',
+    },
+    
+    {
+        id:7,
+        name:'Bertie Yates',
+        age:29,
+        image:'./images/img3.png',
+    }
+
+];
